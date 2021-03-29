8c86ab5a1a7c9ed08440bf1f12be7e3f735a7ea9
Sam Benet <scbenet@gmail.com>
2021-03-28 20:02:39 -0400

    Added README, first test file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SampleRank
 Analysis of samples in music using a modified version of Google's PageRank algorithm
+
+Written in Python 3.9
+
+Dependencies: None so far
+
+Run with 'python3 test.py "Artist Name" "Song Name"
+
+TODO:
+    - Implement database
+    - Implement auto expanding of database
+    - Expand to get songs that sample input
+    - Dockerize project
+    - Actually do pagerank stuff
---
+++ test.py
@@ -0,0 +1,49 @@
+import bs4
+import urllib3
+import re
+import html
+import sys
+
+
+def get_samples_from_page(url):
+
+    # Gets html from the specified page
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+
+    # Converts data to string and isolates relevant part of page
+    htm = str(r.data)
+    matc = []
+    if "Contains samples" in htm:
+        htm = htm.split("Contains samples")[1]
+        htm = htm.split("Was sampled")[0]
+        htm = htm.split("Was covered")[0]
+
+        # Gets list of samples in the song
+        matches = re.findall('title=".*?"', htm)
+        matc = matc + list(set(matches))
+
+        matc = list(filter((lambda x: "\\'s" in x), matc))
+
+        for n in range(len(matc)):
+            matc[n] = matc[n].removeprefix("title=\"")
+            matc[n].removesuffix("\"")
+            re.sub(r'\W+', '', matc[n])
+            matc[n] = html.unescape(matc[n])
+
+        for mat in matc:
+            print(mat)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    artist = args[1]
+    song_title = args[2]
+
+    artist = artist.replace(" ", "-")
+    song_title = song_title.replace(" ", "-")
+    ws_url = f'https://www.whosampled.com/{artist}/{song_title}'
+    print(ws_url)
+    get_samples_from_page(ws_url)
+
